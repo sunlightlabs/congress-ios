3871a32115d47182f832e61b2f4b569bc00c9e45
Daniel Cloud <dcloud@sunlightfoundation.com>
2013-02-25 12:51:34 -0500

    Added UIView+ViewGeometryAdditions category to simplify view layout code.

---
+++ Classes/Categories/UIView+ViewGeometryAdditions.h
@@ -0,0 +1,22 @@
+//
+//  UIView+ViewGeometryAdditions.h
+//  Congress
+//
+//  Created by Daniel Cloud on 2/25/13.
+//  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ViewGeometryAdditions)
+
+@property CGPoint origin;
+@property CGSize size;
+@property CGFloat height;
+@property CGFloat width;
+@property CGFloat top;
+@property CGFloat left;
+@property CGFloat bottom;
+@property CGFloat right;
+
+@end
---
+++ Classes/Categories/UIView+ViewGeometryAdditions.m
@@ -0,0 +1,109 @@
+//
+//  UIView+ViewGeometryAdditions.m
+//  Congress
+//
+//  Created by Daniel Cloud on 2/25/13.
+//  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
+//
+
+#import "UIView+ViewGeometryAdditions.h"
+
+@implementation UIView (ViewGeometryAdditions)
+
+- (CGPoint)origin
+{
+    return self.frame.origin;
+}
+
+- (void)setOrigin:(CGPoint)origin
+{
+    CGRect editFrame = self.frame;
+    editFrame.origin = origin;
+    self.frame = editFrame;
+}
+
+- (CGSize)size
+{
+    return self.frame.size;
+}
+
+- (void)setSize:(CGSize)size
+{
+    CGRect editFrame = self.frame;
+    editFrame.size = size;
+    self.frame = editFrame;
+}
+
+- (CGFloat)height
+{
+    return self.frame.size.height;
+}
+
+- (void)setHeight:(CGFloat)height
+{
+    CGRect editFrame = self.frame;
+    editFrame.size.height = height;
+    self.frame = editFrame;
+}
+
+- (CGFloat)width
+{
+    return self.frame.size.width;
+}
+
+- (void)setWidth:(CGFloat)width
+{
+    CGRect editFrame = self.frame;
+    editFrame.size.width = width;
+    self.frame = editFrame;
+}
+
+- (CGFloat)top
+{
+    return self.frame.origin.y;
+}
+
+- (void)setTop:(CGFloat)top
+{
+    CGRect editFrame = self.frame;
+    editFrame.origin.y = top;
+    self.frame = editFrame;
+}
+
+- (CGFloat)left
+{
+    return self.frame.origin.x;
+}
+
+- (void)setLeft:(CGFloat)left
+{
+    CGRect editFrame = self.frame;
+    editFrame.origin.x = left;
+    self.frame = editFrame;
+}
+
+- (CGFloat)bottom
+{
+    return self.frame.origin.y + self.frame.size.height;
+}
+
+- (void)setBottom:(CGFloat)bottom
+{
+    CGRect editFrame = self.frame;
+    editFrame.origin.y = bottom - self.frame.size.height;
+    self.frame = editFrame;
+}
+
+- (CGFloat)right
+{
+    return self.frame.origin.x + self.frame.size.width;
+}
+
+- (void)setRight:(CGFloat)right
+{
+    CGRect editFrame = self.frame;
+    editFrame.origin.x = right - self.frame.size.width;
+    self.frame = editFrame;
+}
+
+@end
