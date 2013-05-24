17b11dbb602c590540d52b0ed2caabc884220bda
Daniel Cloud <dcloud@sunlightfoundation.com>
2013-05-24 15:12:13 -0400

    Updated share text on info screen to refer to @SunFoundation

--- Classes/Controllers/SFSettingsSectionViewController.m
+++ Classes/Controllers/SFSettingsSectionViewController.m
@@ -33,7 +33,7 @@
         self.title = @"Info";
         _settingsView = [[SFSettingsSectionView alloc] initWithFrame:CGRectZero];
         _shareableObjects = [NSMutableArray array];
-        [_shareableObjects addObject:@"Keep tabs on Capitol Hill: Use the Sunlight Foundation's Congress app to follow bills, contact legislators and more."];
+        [_shareableObjects addObject:@"Keep tabs on Capitol Hill: Use @SunFoundation's Congress app to follow bills, contact legislators and more."];
         [_shareableObjects addObject:[SFCongressURLService globalLandingPage]];
 
     }
