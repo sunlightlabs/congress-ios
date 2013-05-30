8ba8cbdca6f8fdf5d42ee3194382be1370fca7c2
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-05-30 17:23:16 -0400

    Add TSMessage subclass that will eventually contain custom notification stuff.

---
+++ Classes/Misc/SFMessage.h
@@ -0,0 +1,13 @@
+//
+//  SFMessage.h
+//  Congress
+//
+//  Created by Jeremy Carbaugh on 5/30/13.
+//  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
+//
+
+#import "TSMessage.h"
+
+@interface SFMessage : TSMessage
+
+@end
---
+++ Classes/Misc/SFMessage.m
@@ -0,0 +1,14 @@
+//
+//  SFMessage.m
+//  Congress
+//
+//  Created by Jeremy Carbaugh on 5/30/13.
+//  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
+//
+
+#import "SFMessage.h"
+#import <TSMessageView.h>
+
+@implementation SFMessage
+
+@end
