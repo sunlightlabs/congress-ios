c40d2e6af88b30dd1efe12def102b0fdf9b96094
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-05-31 12:39:43 -0400

    Add app-level settings manager.

---
+++ Classes/SFAppSettings.h
@@ -0,0 +1,13 @@
+//
+//  SFAppSettings.h
+//  Congress
+//
+//  Created by Jeremy Carbaugh on 5/31/13.
+//  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFAppSettings : NSObject
+
+@end
---
+++ Classes/SFAppSettings.m
@@ -0,0 +1,13 @@
+//
+//  SFAppSettings.m
+//  Congress
+//
+//  Created by Jeremy Carbaugh on 5/31/13.
+//  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
+//
+
+#import "SFAppSettings.h"
+
+@implementation SFAppSettings
+
+@end
