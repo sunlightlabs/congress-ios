484c6827dca0cd363a835a19687635b952fedb3c
Daniel Cloud <dcloud@sunlightfoundation.com>
2013-06-06 16:17:44 -0400

    Use questionShort in SFRollCallVoteByLegislatorCellTransformer. Fixed lack of font definition for detailTextLabelFont.

--- Classes/Transformers/SFRollCallVoteByLegislatorCellTransformer.m
+++ Classes/Transformers/SFRollCallVoteByLegislatorCellTransformer.m
@@ -12,6 +12,7 @@
 #import "SFOpticView.h"
 #import "SFRollCallVote.h"
 #import "SFLegislator.h"
+#import "SFBill.h"
 
 @implementation SFRollCallVoteByLegislatorCellTransformer
 
@@ -35,30 +36,33 @@
     cellData.cellIdentifier = @"SFRollCallVoteByLegislatorCell";
     cellData.cellStyle = UITableViewCellStyleSubtitle;
 
-    cellData.textLabelString = vote.question;
+    cellData.textLabelString = vote.questionShort;
 //    cellData.textLabelString = [NSString stringWithFormat:@"Voted '%@' on '%@'", legislatorsVote, vote.question];
     cellData.textLabelFont = [UIFont cellTextFont];
     cellData.textLabelColor = [UIColor primaryTextColor];
     cellData.textLabelNumberOfLines = 4;
 
-    id forCount = vote.totals[@"Yea"]?: vote.totals[@"Guilty"];
-    id againstCount = vote.totals[@"Nay"]?: vote.totals[@"Not Guilty"];
-    NSMutableString *voteDetail = [NSMutableString stringWithString:vote.result];
-    if (forCount && againstCount) {
-        NSString *forLabel = vote.totals[@"Yea"] ? @"Yea" : @"Guilty";
-        NSString *againstLabel = vote.totals[@"Nay"] ? @"Nay" : @"Not Guilty";
-        [voteDetail appendFormat:@": %@ \u2018%@\u2019 to %@ \u2018%@\u2019", forCount, forLabel, againstCount, againstLabel];
-    }
-    
-    cellData.detailTextLabelString = voteDetail;
+    cellData.detailTextLabelString = vote.billId;
+    cellData.detailTextLabelFont = [UIFont cellDetailTextFont];
     cellData.detailTextLabelColor = [UIColor secondaryTextColor];
     cellData.detailTextLabelNumberOfLines = 1;
 
     cellData.extraData = [NSMutableDictionary dictionary];
     SFOpticView *view = [[SFOpticView alloc] initWithFrame:CGRectZero];
-    NSMutableAttributedString *voteDescription = [[NSMutableAttributedString alloc] initWithString:@"Voted " attributes:@{NSForegroundColorAttributeName: [UIColor secondaryTextColor]}];
-    NSAttributedString *legislatorVoteString = [[NSAttributedString alloc] initWithString:legislatorsVote attributes:@{NSForegroundColorAttributeName: [UIColor primaryTextColor], NSFontAttributeName: [UIFont cellPanelStrongTextFont]}];
-    [voteDescription appendAttributedString:legislatorVoteString];
+
+    NSMutableAttributedString *voteDescription = [[NSMutableAttributedString alloc] initWithString:legislatorsVote attributes:@{NSForegroundColorAttributeName: [UIColor primaryTextColor], NSFontAttributeName: [UIFont cellPanelStrongTextFont]}];
+
+    id forCount = vote.totals[@"Yea"]?: vote.totals[@"Guilty"];
+    id againstCount = vote.totals[@"Nay"]?: vote.totals[@"Not Guilty"];
+    NSMutableAttributedString *voteDetail = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"\n%@", vote.result]];
+    if (forCount && againstCount) {
+        NSString *forLabel = vote.totals[@"Yea"] ? @"Yea" : @"Guilty";
+        NSString *againstLabel = vote.totals[@"Nay"] ? @"Nay" : @"Not Guilty";
+        NSAttributedString *resultDescription = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@": %@ \u2018%@\u2019 to %@ \u2018%@\u2019", forCount, forLabel, againstCount, againstLabel]];
+        [voteDetail appendAttributedString:resultDescription];
+    }
+    [voteDescription appendAttributedString:voteDetail];
+
     view.textLabel.attributedText = voteDescription;
     [cellData.extraData setObject:@[view] forKey:@"opticViews"];
     cellData.extraHeight = SFOpticViewHeight + SFOpticViewMarginVertical;
