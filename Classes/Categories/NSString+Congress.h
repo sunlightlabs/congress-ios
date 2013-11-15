84dfdbeb9ba693211f8e0b6f3930051fe3c9518e
Daniel Cloud <dcloud@sunlightfoundation.com>
2013-11-15 11:20:44 -0500

    Remove homemade ordinalnumber method cause FormatterKit.

--- Classes/Categories/NSString+Congress.h
+++ Classes/Categories/NSString+Congress.h
@@ -4,14 +4,12 @@
 //
 //  Created by Daniel Cloud on 3/19/13.
 //  Copyright (c) 2013 Sunlight Foundation. All rights reserved.
-//  Adapted from http://stackoverflow.com/questions/3312935/nsnumberformatter-and-th-st-nd-rd-ordinal-number-endings 
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSString (Congress)
 
-+ (NSString *)ordinalFromNumber:(NSNumber *)number;
 - (CGSize)sf_sizeWithFont:(UIFont *)font constrainedToSize:(CGSize)size;
 
 @end
--- Classes/Categories/NSString+Congress.m
+++ Classes/Categories/NSString+Congress.m
@@ -10,37 +10,6 @@
 
 @implementation NSString (Congress)
 
-+ (NSString*)ordinalFromNumber:(NSNumber *)number
-{
-    NSString *ending;
-    NSInteger num = [number integerValue];
-
-    int ones = num % 10;
-    int tens = floor(num / 10);
-    tens = tens % 10;
-
-    if(tens == 1){
-        ending = @"th";
-    } else {
-        switch (ones) {
-            case 1:
-                ending = @"st";
-                break;
-            case 2:
-                ending = @"nd";
-                break;
-            case 3:
-                ending = @"rd";
-                break;
-            default:
-                ending = @"th";
-                break;
-        }
-    }
-
-    return [NSString stringWithFormat:@"%d%@", num, ending];
-}
-
 #pragma mark - iOS 7 bifurcation method
 - (CGSize)sf_sizeWithFont:(UIFont *)font constrainedToSize:(CGSize)size
 {
