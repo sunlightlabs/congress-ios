bbef31edbd681befe54a9c48831d83baf22a0b40
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-11-04 17:29:01 -0500

    Set initial RMMapboxSource in initWithRetinaSupport initializer so we can initialize with our tileset (otherwise a default tileset gets loaded in the moments before getting switched to our tileset). Towards improvements in #424.

--- Classes/Views/SFMapView.m
+++ Classes/Views/SFMapView.m
@@ -12,15 +12,17 @@
     BOOL _isRetina;
 }
 
-@synthesize borderLine = _borderLine;
-@synthesize offlineTileSource = _offlineTileSource;
-@synthesize onlineTileSource = _onlineTileSource;
-@synthesize isOnline = _isOnline;
+@synthesize borderLine;
+@synthesize offlineTileSource;
+@synthesize onlineTileSource;
+@synthesize isOnline;
 
 - (id)initWithRetinaSupport {
     _isRetina = [[UIScreen mainScreen] scale] > 1.0;
-    self = [self initWithFrame:CGRectMake(0.0, 0.0, 1.0, 1.0)];
+    RMMapboxSource *tileSource = [[RMMapboxSource alloc] initWithMapID:_isRetina ? @"sunfoundation.map-3l6khrw5":@"sunfoundation.map-f10t1goc"];
+    self = [self initWithFrame:CGRectMake(0.0, 0.0, 1.0, 1.0) andTilesource:tileSource];
     if (self) {
+        self.onlineTileSource = tileSource;
         [self _initialize];
     }
     return self;
@@ -35,9 +37,9 @@
 }
 
 - (void)_initialize {
-    _borderLine = [[SFLineView alloc] initWithFrame:CGRectMake(0, 0, self.width, 1.0f)];
-    _borderLine.lineColor = [UIColor mapBorderLineColor];
-    [self addSubview:_borderLine];
+    self.borderLine = [[SFLineView alloc] initWithFrame:CGRectMake(0, 0, self.width, 1.0f)];
+    self.borderLine.lineColor = [UIColor mapBorderLineColor];
+    [self addSubview:self.borderLine];
 
 
     self.showLogoBug = NO;
@@ -55,20 +57,22 @@
     if ([self.tileSources count] > 0) {
         [self removeTileSourceAtIndex:0];
     }
-    _isOnline = NO;
+    self.isOnline = NO;
 }
 
 - (void)useOnlineTiles {
-    if (_onlineTileSource == nil) {
-        _onlineTileSource = [[RMMapboxSource alloc] initWithMapID:_isRetina ? @"sunfoundation.map-3l6khrw5":@"sunfoundation.map-f10t1goc"];
+    if (self.onlineTileSource == nil) {
+        self.onlineTileSource = [[RMMapboxSource alloc] initWithMapID:_isRetina ? @"sunfoundation.map-3l6khrw5":@"sunfoundation.map-f10t1goc"];
     }
-    [self setTileSource:_onlineTileSource];
+    if (self.tileSource != self.onlineTileSource) {
+        self.tileSource = self.onlineTileSource;
+    }
     [self setAdjustTilesForRetinaDisplay:NO];
-    _isOnline = YES;
+    self.isOnline = YES;
 }
 
 - (NSInteger)maximumZoom {
-    return _isOnline ? 20 : 7;
+    return self.isOnline ? 20 : 7;
 }
 
 #pragma mark - UIAccessibility
